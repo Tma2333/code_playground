26107bc296cc474fc2f21cf8e463d034150aa5e8
Yuntao Ma <tommyma0402@gmail.com>
2019-05-28 17:01:51 -0400

    finish adding args parser and devices search

--- usb_auto-detect/usb_auto-detect.py
+++ usb_auto-detect/usb_auto-detect.py
@@ -5,7 +5,14 @@ import os
 
 usb_dev_path = '/sys/bus/usb/devices/'
 
-def usb_dev_list():
+parser = argparse.ArgumentParser(description='Search for all USB devices.')
+parser.add_argument('-l', '--list', action='store_true',
+                  help='showing list of all devices')
+parser.add_argument('-f', '--find', type=str, metavar = 'NAME',
+                  help='show all matching devices')
+args = parser.parse_args()
+
+def usb_dev_list(out = False):
     dev_list = [[], []]
     for bus in os.listdir(usb_dev_path):
         if 'usb' in bus: 
@@ -25,10 +32,35 @@ def usb_dev_list():
                                         key = param[0]
                                         value = param[1]
                                         if 'NAME' in key:
-                                            dev_name = value
+                                            dev_name = value.strip('/"')
                                             dev_list[1].append(dev_name)
+    if out:
+        for i in range(len(dev_list[0])):
+            print('{}: {}'.format(dev_list[0][i], dev_list[1][i]))
     return dev_list
 
-print(usb_dev_list())
+def find_usb_dev (dev_name, out = False):
+    dev_list = usb_dev_list(False)
+    index = []
+    for i in range(len(dev_list[0])):
+        if dev_name.lower() in dev_list[1][i].lower():
+            index.append(i)
+    if len(index) == 0:
+        print('No matching USB devices found')
+        return None
+    if out:
+        print('Found {} matching result:'.format(len(index)))
+        for i in index:
+            print('{}: {}'.format(dev_list[0][i], dev_list[1][i]))
+    return [(dev_list[0][i], dev_list[1][i]) for i in index]
+
+
+if args.list:
+    usb_dev_list(out = True)
+if args.find:
+    find_usb_dev(args.find, out = True)
+
+
+
 
         
