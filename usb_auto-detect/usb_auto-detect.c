963441dd839b67c7482aacc94ab87327c71ec9ba
Yuntao Ma <tommyma0402@gmail.com>
2019-05-30 16:50:23 -0400

    start working on directory walk

--- usb_auto-detect/usb_auto-detect.c
+++ usb_auto-detect/usb_auto-detect.c
@@ -1,21 +1,48 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <dirent.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
-/*
-char* usbDevList (){
+struct device{
+    char* devAddr;
+    char* devName;
+    struct device* next;
+};
+
+void locUSBdev(const char *dirp, struct device *current){
+    struct dirent *ent;
+    DIR *dir;
+    struct stat s;
+    
+    if (!strstr(dirp, "/sys/bus/usb/dev/devices/usb")){
+        if ((dir = opendir(dirp)) != NULL){
+            while ((ent = readdir(dir)) != NULL){
+                if (strcmp(ent->d_name, ".") != 0 && strcmp(ent->d_name, "..") != 0 ){
+                    
+                }
+            }
+        }
+    }
+}
+
+
+
+void usbDevList(struct device *head){
+    // variables
+    struct dirent *ent;
+    DIR *dir;
+
+    // checking all folder under 
+    dir = opendir("/sys/bus/usb/devices/");
     
 
 }
-*/
+
 
 int main(){
-    struct dirent *de;
-
-    DIR *dr = opendir("/sys/bus/usb/devices/");
-
-    while ((de = readdir(dr)) != NULL)
-        printf("%s\n", de->d_name);
-
-
+    struct device* head = NULL;
+    
     return 0;
 }
