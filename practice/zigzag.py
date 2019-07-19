8c4930f9c81948dbd1fc14313b4efa970df03630
Yuntao Ma <tommyma0402@gmail.com>
2019-07-19 09:10:09 -0400

    day6, might improve later

---
+++ practice/zigzag.py
@@ -0,0 +1,35 @@
+#!usr/bin/python3
+
+class solution:
+    def sol1(self, s, num_row):
+        zigzag = ''
+        if num_row == 1:
+            return s
+        for i in range (num_row):
+            if len(zigzag) == len(s):
+                break
+            index = i
+            row_factor = i*2
+            factor1 = 0+row_factor
+            factor2 = num_row*2-2-row_factor
+            while True:
+                if factor2 > 0:
+                    zigzag += s[index]
+                    index += factor2
+                    if index >= len(s):
+                        break
+                if factor1 > 0:
+                    zigzag += s[index]
+                    index += factor1
+                    if index >= len(s):
+                        break
+
+        return zigzag
+
+
+def test():
+    sol = solution()
+    zigzag = sol.sol1('a', 2)
+    print(zigzag)
+
+test() 
