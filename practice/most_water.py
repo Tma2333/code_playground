a7c7e3469cc110d001bb39d2cfd5fc5c2007e0c6
Yuntao Ma <tommyma0402@gmail.com>
2019-07-30 03:13:29 -0400

    day 10-two pointer

--- practice/most_water.py
+++ practice/most_water.py
@@ -9,7 +9,20 @@ class solution:
                 if area>maxA:
                     maxA = area
         return maxA
+    
+    def sol2(self, height):
+        maxA, left, right = 0, 0, len(height)-1
+        while right > left:
+            maxA = max(maxA, min(height[left], height[right])*(right-left))
+
+            if height[left]>height[right]:
+                right -= 1
+            else:
+                left += 1
+        return maxA
 
 sol = solution()
-ans = sol.sol1([1,3,3,1])
+ans = sol.sol1([1,3,3,1,13,21,4,1,21,32,13,2,123,14,123,12,321,321,4,123,21,312,3,123,1,31,412,3,123,21,3123,12,312,31,4,12,3,23])
+print(ans)
+ans = sol.sol2([1,3,3,1,13,21,4,1,21,32,13,2,123,14,123,12,321,321,4,123,21,312,3,123,1,31,412,3,123,21,3123,12,312,31,4,12,3,23])
 print(ans)
