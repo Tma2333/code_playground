7b8b13fde4014a3d67fcbdc218d2ac557915e4eb
Yuntao Ma <tommyma0402@gmail.com>
2019-08-13 11:25:05 -0400

    day 17

--- practice/generate_paren.py
+++ practice/generate_paren.py
@@ -1,3 +1,36 @@
 #!usr/bin/python3
 
 class solutions:
+    def generateParenthesis(self, n: int):
+        if not n:
+            return []
+
+        self.n = n 
+        self.ans = []
+        
+        self.rec(1, 0, '', '(')
+
+        return self.ans
+
+    def rec(self, l, r, s, par):
+        s += par
+
+        if l == self.n and r == self.n:
+            self.ans.append(s)
+            return
+
+        if l == self.n:
+            self.rec(l, r+1, s, ')')
+            return
+
+        if l == r:
+            self.rec(l+1, r, s, '(')
+            return
+
+        if l > r:
+            self.rec(l+1, r, s, '(')
+            self.rec(l, r+1, s, ')')
+            return
+
+ans = solutions()
+print(ans.generateParenthesis(15))
