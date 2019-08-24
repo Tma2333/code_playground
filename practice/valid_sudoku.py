76b60eb606f78f8e51f6d2d3b7a56947221edaad
Yuntao Ma <tommyma0402@gmail.com>
2019-08-24 02:49:57 -0400

    day 22

--- practice/valid_sudoku.py
+++ practice/valid_sudoku.py
@@ -2,6 +2,23 @@
 
 class Solution:
     def isValidSudoku(self, board):
+        row = {}
+        colum = {}
+        block = {}
+
+        for i, x in enumerate(board):
+            for j, y in enumerate(x):
+                if y != '.':
+                    if (i,y) in row or (j,y) in colum or (i//3, j//3, y) in block:
+                        return False
+                    else:
+                        row[i,y] = 1
+                        colum[j,y] = 1
+                        block[i//3, j//3, y] = 1
+            
+            return True
+    
+    def bruteforce(self, board):
         valid = []
         for row in board:
             valid = []
